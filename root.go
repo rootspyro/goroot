63f8a619d3b97c76a53833e04a354d5eb10d4c9c
rootspyro <root.spyro@gmail.com>
2023-03-17 15:27:03 -0400

    Feat: Default statuses list created and Send function for http text responses

--- root.go
+++ root.go
@@ -25,8 +25,74 @@ func(root *Root)Status(code int) *Root {
 	return root
 }
 
+// Default Statuses
+
+// 2XX List
+func(root *Root)OK() *Root {
+	root.Status(200)
+	return root
+}
+
+func(root *Root)Created() *Root {
+	root.Status(201)
+	return root
+}
+
+// 4XX list
+
+func(root *Root)BadRequest() *Root {
+	root.Status(400)
+	return root
+}
+
+func(root *Root)Unauthorized() *Root {
+	root.Status(401)
+	return root
+}
+
+func(root *Root)Forbidden() *Root {
+	root.Status(403)
+	return root
+}
+
+func(root *Root)NotFound() *Root {
+	root.Status(404)
+	return root
+}
+
+func(root *Root)MethodNotAllowed() *Root {
+	root.Status(405)
+	return root
+}
+
+// 5XX List
+
+func(root *Root)InternalServerError() *Root {
+	root.Status(500)
+	return root
+}
+
+func(root *Root)NotImplemented() *Root {
+	root.Status(501)
+	return root
+}
+
 
 // RESPONSES
+
+func(root *Root)Send(data string) {
+
+	httpCode := 200 
+
+	if root._status > 0 {
+		
+		httpCode = root._status
+
+	}
+
+	root.writter.WriteHeader(httpCode)
+	root.writter.Write([]byte(data))
+}
 
 func(root *Root)parseJson(data interface{}) ([]byte, error) {
 	return json.Marshal(data)
